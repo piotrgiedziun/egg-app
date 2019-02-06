cc739729f5a8ef2ec7df6735bafb6d578abe0889
👨‍🚀 Piotr Giedziun <piotrgiedziun@gmail.com>
2019-02-06 12:36:44 +0100

    update docs

---
+++ docs/precache-manifest.c394ab5b305cdd266a77546a18c1998d.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "eec098652827439739a5475f1c1a36dd",
+    "url": "/static/media/topping_strawberries.eec09865.png"
+  },
+  {
+    "revision": "ad3f5fbacc24b4b51221",
+    "url": "/static/css/main.6de1afc3.chunk.css"
+  },
+  {
+    "revision": "d22c2a2c4bcbfb53e0f8ec47afd3f2cf",
+    "url": "/static/media/box_bg.d22c2a2c.png"
+  },
+  {
+    "revision": "076160ab746fe12715e1",
+    "url": "/static/js/1.076160ab.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f89af3fb4f992e03a31cffb7a5c33899",
+    "url": "/static/media/egg_dark.f89af3fb.png"
+  },
+  {
+    "revision": "9031465ab5323216c0113367a6ff0bfe",
+    "url": "/static/media/egg_white.9031465a.png"
+  },
+  {
+    "revision": "bc0ee8176899e41dd46636e8ab4b3e7a",
+    "url": "/static/media/egg_oreo.bc0ee817.png"
+  },
+  {
+    "revision": "2635708255256964a5bb9f1f296e97d3",
+    "url": "/static/media/nutella.26357082.png"
+  },
+  {
+    "revision": "fe250dca38087e79c18daaded2a88ef5",
+    "url": "/static/media/leite_condensado.fe250dca.png"
+  },
+  {
+    "revision": "98d00deb046c58721e9d287c5661be07",
+    "url": "/static/media/white_chocolate.98d00deb.png"
+  },
+  {
+    "revision": "a6ef09c068a97c5cc9126e9008b28e9f",
+    "url": "/static/media/oreo.a6ef09c0.png"
+  },
+  {
+    "revision": "d5c6caeda503ec223be2afc5dc7fa01d",
+    "url": "/static/media/white_cream.d5c6caed.png"
+  },
+  {
+    "revision": "ad3f5fbacc24b4b51221",
+    "url": "/static/js/main.ad3f5fba.chunk.js"
+  },
+  {
+    "revision": "e99b974ad67a152b3e11e542de063668",
+    "url": "/static/media/topping_mms.e99b974a.png"
+  },
+  {
+    "revision": "9a3fe3c2df196fb0f4e0ec5c917af718",
+    "url": "/static/media/topping_oreo.9a3fe3c2.png"
+  },
+  {
+    "revision": "8f71b4762c2dbe6a8719af0f8f2a101f",
+    "url": "/static/media/topping_blueberries.8f71b476.png"
+  },
+  {
+    "revision": "69940d26e8cc9e981536ae440a574b9c",
+    "url": "/static/media/topping_banana.69940d26.png"
+  },
+  {
+    "revision": "74aaf9b949a67b02a619bd86074b9eca",
+    "url": "/static/media/topping_candies.74aaf9b9.png"
+  },
+  {
+    "revision": "1bec8165e23177001760da498cf146e5",
+    "url": "/static/media/topping_bears.1bec8165.png"
+  },
+  {
+    "revision": "981f63cf108fd1152ed4e091797e61fe",
+    "url": "/static/media/topping_love.981f63cf.png"
+  },
+  {
+    "revision": "6fa925058314f771aa8c0d72445c55be",
+    "url": "/static/media/final_2.6fa92505.png"
+  },
+  {
+    "revision": "0405941b8d04f4f4c7131830919ebdcc",
+    "url": "/static/media/final_3.0405941b.png"
+  },
+  {
+    "revision": "8da9a5fd058ac2e794f28a5d0244fc36",
+    "url": "/static/media/final_4.8da9a5fd.png"
+  },
+  {
+    "revision": "ff199a475a33f6ecf0459be0e91f2aa4",
+    "url": "/static/media/final_1.ff199a47.png"
+  },
+  {
+    "revision": "076160ab746fe12715e1",
+    "url": "/static/css/1.7a10b6ac.chunk.css"
+  },
+  {
+    "revision": "27a38bc5a1e6813d299be047d7a4dd37",
+    "url": "/index.html"
+  }
+];
